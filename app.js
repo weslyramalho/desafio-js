719eb94acbabfcc3be4b1f4d727648e2107c370d
weslyramalho <weslyramalho@yahoo.com.br>
2024-01-19 15:32:43 -0300

    finalizado

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 alert("Boas  vindas ao nosso site!")
+
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert("Erro! Preencha todos os campos");
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
+
+nome = prompt("Qual é o seu nome? ")
+idade = prompt("Idade? ");
+
+if (idade >= 18){
+    alert ("Pode tirar a habilitação!");
+}
+else{
+    alert("Ainda não pode tirar a habilitação!");
+}
